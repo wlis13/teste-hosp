613f9d97773c361f8d36371de2ca09138bb4dfec
wlisses <wlissesfernando285@gmail.com>
2023-03-04 17:17:39 -0300

    baixei http-status-codes

--- src/controllers/carnesController.js
+++ src/controllers/carnesController.js
@@ -1,9 +1,10 @@
 const { carneService } = require('../services');
+const { StatusCodes } = require('http-status-codes');
 
 const getAll = async (_req, res) => {
   try {
     const result = await carneService.findAll();
-    return res.status(200).json(result);
+    return res.status(StatusCodes.ACCEPTED).json(result);
   } catch (error) {
     console.log(error.message);
   }
